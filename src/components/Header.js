d9e1ec2830a80038eecca001bc890e7a1a7735f9
Sean McPherson <seanmcp25@gmail.com>
2017-09-18 15:20:32 -0400

    Style Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <nav>Navigation</nav>
+      <nav className="jumbotron">
+        <h1 className="display-1">Navigation</h1>
+        <h6 className="text-muted">Find your true north</h6>
+      </nav>
     );
   }
 }
