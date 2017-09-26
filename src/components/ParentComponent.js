59cd434e2370e7e98e09ccbdb599d5506c21cd90
Sean McPherson <seanmcp25@gmail.com>
2017-09-26 17:26:28 -0400

    Reset form on submit

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -29,9 +29,8 @@ export default class ParentComponent extends Component {
     //check console to see if firing
     console.log("fired");
     //set the state for props and for value (prevents output from appearing when typing)
-    this.setState({whatToSay: this.state.whatToSay, whatWasSaid: this.state.whatToSay});
+    this.setState({whatToSay: '', whatWasSaid: this.state.whatToSay});
     //clear our input by resetting state
-    this.setState({whatToSay: ""});
     document.getElementById("messageCard").classList.remove("hide");
   }
   render() {
@@ -40,7 +39,7 @@ export default class ParentComponent extends Component {
       <div className="w-75 mx-auto d-flex">
         <div className="form-group text-left w-50 m-4">
           <label htmlFor="sayWhat">What do you have to say?</label>
-          <input className="form-control" id="sayWhat" aria-describedby="sayWhatHelp" onChange={this.handleInput} type="text" placeholder="Say It, Don't Spray It!" />
+          <input className="form-control" id="sayWhat" aria-describedby="sayWhatHelp" onChange={this.handleInput} type="text" placeholder="Say It, Don't Spray It!" value={this.state.whatToSay}/>
           <small id="sayWhatHelp" className="form-text text-muted">We want to hear from you</small>
           <ChildComponent onClick={this.handleSubmit}/>
         </div>
